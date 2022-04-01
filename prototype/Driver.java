28d4797eca499277032d2be5969051d33bd62d31
austinjhoward <austinjhoward00@gnail.com>
2022-04-01 12:37:12 -0500

    GUI hello world

--- prototype/Driver.java
+++ prototype/Driver.java
@@ -1,11 +1,23 @@
 /** 
  * My first Java program
  */
+import javax.swing.*;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
 
-public class HelloWorld
+
+public class Driver
 {
+
    public static void main(String []args)
    {
-      System.out.println("Hello World");
+      	JFrame frame = new JFrame("My First GUI");
+       frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+       frame.setSize(300,300);
+       JTextField textField = new JTextField("Hello world!");
+       textField.setBounds(50, 100, 200, 30);
+       frame.add(textField);
+       frame.setLayout(null);
+       frame.setVisible(true);
    }
 }
