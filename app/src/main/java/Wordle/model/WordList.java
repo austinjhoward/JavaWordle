bdd38a91817f36199f39156d525f4920e866ae84
Homebase 2 <homebase2@Kellys-MacBook-Pro.local>
2022-04-07 16:18:39 -0500

    Adding models

---
+++ app/src/main/java/Wordle/model/WordList.java
@@ -0,0 +1,15 @@
+package Wordle.model;
+
+import java.util.List;
+
+public class WordList 
+{
+    private List<String> words = new List<String>();
+
+    public Wordlist(List<String>) 
+    {
+        
+    }
+
+    
+}
