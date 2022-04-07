fca66209136ff7df9bd0974b3be999acc972679f
Homebase 2 <homebase2@Kellys-MacBook-Pro.local>
2022-04-07 16:52:27 -0500

    created WordList

--- app/src/main/java/Wordle/model/WordList.java
+++ app/src/main/java/Wordle/model/WordList.java
@@ -1,15 +1,29 @@
 package Wordle.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class WordList 
 {
-    private List<String> words = new List<String>();
+    private List<String> words = new ArrayList<String>();
 
-    public Wordlist(List<String>) 
+    public WordList(ArrayList<String> words) 
     {
-        
+        System.out.println("WordList method used");
     }
 
-    
+    public String getWordRandomly()
+    {
+        System.out.println("getWordRandomly method used");
+        String word = "A random word";
+        return word;
+    }
+
+    public Boolean existsInList(String word)
+    {
+        System.out.println("existsInList method used");
+        return true;
+    }
 }
+    
+
