91891cf42be08b84260f061657137e66f075f2e3
Homebase 2 <homebase2@Kellys-MacBook-Pro.local>
2022-04-07 15:19:46 -0500

    Adding gradle

--- classExperimentation/KellyDriver.java
+++ classExperimentation/KellyDriver.java
@@ -5,20 +5,19 @@ import javax.swing.*;
 import java.awt.BorderLayout;
 import java.lang.String;
 
-
 public class KellyDriver
 {
 
    public static void main(String []args)
    {
-      	JFrame frame = new JFrame("Wordle");
-       frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-       frame.setSize(600,800);
+      JFrame frame = new JFrame("Wordle");
+      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      frame.setSize(600,800);
 
-       JPanel masterPanel = new JPanel(new BorderLayout());
-       WordleGrid gridPanel = new WordleGrid();
-       masterPanel.add(gridPanel);
-       frame.add(masterPanel);
-       frame.setVisible(true);
+      JPanel masterPanel = new JPanel(new BorderLayout());
+      WordleGrid gridPanel = new WordleGrid();
+      masterPanel.add(gridPanel, BorderLayout.CENTER);
+      frame.getContentPane().add(masterPanel);
+      frame.setVisible(true);
    }
 }
--- classExperimentation/WordleGrid.java
+++ classExperimentation/WordleGrid.java
@@ -1,24 +1,41 @@
 import javax.swing.*;
 import javax.swing.border.Border;
 import javax.swing.border.LineBorder;
-import java.util.ArrayList;
 import java.awt.*;
 
 
 public class WordleGrid extends JPanel
 {
-    public ArrayList<JLabel> labelList = new ArrayList<JLabel>();
+    private JLabel labels[] = new JLabel[30];
+    private JPanel panels[] = new JPanel[30];
+    private Border border = LineBorder.createGrayLineBorder();
+    private Font font1 = new Font("Arial", Font.BOLD, 80);
+
+    // public ArrayList<JLabel> labelList = new ArrayList<JLabel>();
     public WordleGrid() 
     {
         JPanel guessGrid = new JPanel();
         guessGrid.setLayout(new GridLayout(6,5,2,2));
-        Border border = LineBorder.createGrayLineBorder();
         for(int i = 0; i < 30; i++)
         {
-            JLabel label = new JLabel(" ");
-            label.setBorder(border);
-            labelList.add(label);
-            guessGrid.add(label);
+            labels[i] = new JLabel(" ",JLabel.CENTER);
+            panels[i] = new JPanel();
+            labels[i].setFont(font1);
+            labels[i].setBorder(border);
+            labels[i].setForeground(new Color(238,238,238));
+            panels[i].setBackground(new Color(198,181,102));
+            panels[i].add(labels[i]);
+            guessGrid.add(panels[i]);
+        }
+
+        //JPanel guessGrid = new JPanel();
+        //guessGrid.setLayout(new GridLayout(6,5,2,2));
+        //for(int i = 0; i < 30; i++)
+        {
+            //JLabel label = new JLabel(" ");
+            //label.setBorder(border);
+            //labelList.add(label);
+            //guessGrid.add(label);
         }
     }
 }
