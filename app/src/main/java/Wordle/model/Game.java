40e6073152b30dbb2b3c4523524172c002237ac2
Homebase 2 <homebase2@Kellys-MacBook-Pro.local>
2022-04-13 11:57:29 -0500

    Adding game model

---
+++ app/src/main/java/Wordle/model/Game.java
@@ -0,0 +1,46 @@
+package Wordle.model;
+
+import java.util.ArrayList;
+
+public class Game 
+{
+    private int roundNumber;
+    private ArrayList<Integer> currentPositions = new ArrayList<Integer>();
+    private int maxRounds;
+
+    public Boolean hasWon()
+    {
+        System.out.println("hasWon method called");
+        return true;
+    }
+
+    public Boolean hasLost()
+    {
+        System.out.println("hasLost method called");
+        return false;
+    }
+
+    public int getRoundNumber()
+    {
+        System.out.println("getRoundNumber called");
+        return this.roundNumber;
+    }
+
+    public ArrayList<Integer> getCurrentPosition()
+    {
+        System.out.println("getCurrentPosition called");
+        return this.currentPositions;
+    }
+
+    public void incRoundNumber()
+    {
+        System.out.println("Round nunber increased");
+        this.roundNumber ++;
+    }
+
+    public void setMaxRounds(int max)
+    {
+        System.out.println("setMaxRounds used");
+        this.roundNumber = max;
+    }
+}
