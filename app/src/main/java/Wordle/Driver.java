297e6f2a1be2f5b2833482eb621ffea593910597
austinjhoward <austinjhoward00@gnail.com>
2022-04-19 20:10:58 -0500

    Driver updates for P2D2

--- app/src/main/java/Wordle/Driver.java
+++ app/src/main/java/Wordle/Driver.java
@@ -1,12 +1,55 @@
 //User interface prototype for wordle
 package Wordle;
 
-import Wordle.view.WordleGUI;
+import Wordle.view.*;
+import Wordle.model.*;
+import Wordle.controller.*;
+import java.awt.event.*;
+import java.io.IOException;
 
 public class Driver {
 
    public static void main(String[] args) { // Creates a 2D array of Jpanels, and of JLabels
 
-      WordleGUI gui = new WordleGUI();
+      DictionaryLoader dictionary = new DictionaryLoader();
+
+      try {
+         WordList wordList = new WordList(dictionary.loadDictionary("Dictionary.txt"));
+         WordleGUI gui = new WordleGUI();
+         WordleController controller = new WordleController(dictionary, wordList, gui);
+
+         gui.setEnter(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+               controller.onEnter();
+            }
+         });
+
+         gui.setOnChangeText(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+               controller.onChangeText();
+            }
+         });
+
+         gui.setEasy(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+               controller.onEasy();
+            }
+         });
+
+         gui.setMedium(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+               controller.onMedium();
+            }
+         });
+
+         gui.setHard(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+               controller.onHard();
+            }
+         });
+      } catch (IOException e) {
+         System.out.println(e);
+      }
+
    }
 }
