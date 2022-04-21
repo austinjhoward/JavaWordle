2272c1d0fcd7b2f08a7b03059e1ea6641aef9e12
Carmen <morerac@slu.edu>
2022-04-21 11:09:05 -0500

    New directory to try out CheckPositions

---
+++ CheckPositionsTesting/CheckPositionsTesting.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+import java.lang.Integer;
+
+public class CheckPositionsTesting {
+
+public static void main(String args[]){
+
+//Write two 5 letter words (all lowercase)
+//compile and run.
+//the output should be 0 for the letters that will be gray, 1 for yellow and 2 for green 
+        String word = "abcde";
+        String hiddenWord = "bcdea";
+        ArrayList<Integer> positions = new ArrayList<Integer>();
+        ArrayList<Integer> indexesMatching = new ArrayList<Integer>();
+        for(int i = 0; i < 5; i++)
+        {
+            if(word.charAt(i) == hiddenWord.charAt(i))
+            {
+                positions.add(2);
+                indexesMatching.add(i);
+            }
+            else 
+            {
+                positions.add(0);
+            }
+        }
+        
+        for(int i = 0; i < 5; i++)
+        {
+            Boolean exists = false;
+            for(int j = 0; j < 5; j++)
+            {
+                if(!indexesMatching.contains(j))
+                {
+                    if(word.charAt(i) == hiddenWord.charAt(j) && !exists)
+                    {
+                        exists = true;
+                        positions.set(i,1);
+                        indexesMatching.add(j);
+                    }
+                }
+            }
+        }
+        System.out.println(positions);
+}
+}
